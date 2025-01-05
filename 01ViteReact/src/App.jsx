79e21768a4aa49e83a2d43ed6522c11b9ed86d5a
Manish Khadka <mrsomeonne@gmail.com>
2025-01-05 17:30:17 +0545

    Learned Custom React by Creating our own Library

--- 01ViteReact/src/App.jsx
+++ 01ViteReact/src/App.jsx
@@ -1,14 +1,18 @@
 import One from "./One";
 
 function App() {
+
+  const username = "Someonne"
+
   return (
     // <h1>Hello World In VITE</h1>
     // can only export a single element
     // You can export multiple elements using div or using fragment (empty element) <>.
 
     <>
-      <h1>Hello World</h1>
+      <h1>Hello World, Hello: {username}</h1>
       <One />
+      <p>Test Paragraph</p>
     </>
   );
 }
--- 01ViteReact/src/main.jsx
+++ 01ViteReact/src/main.jsx
@@ -1,9 +1,55 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+/* eslint-disable no-unused-vars */
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-)
+function MyApp() {
+  return (
+    <div>
+      <h1>Custom App Practice !</h1>
+    </div>
+  );
+}
+
+/* const ReactElement = {
+  type: 'a',
+  props: {
+    href: 'https://youtube.com',
+    target: '_blank'
+  },
+  children: 'Click Here To Open Youtube'
+} */
+
+const anotherElement = (
+  <a href="https://youtube.com" target="_blank">
+    Visit Google
+  </a>
+);
+
+//Declaring Variable
+const anotherUser = " Nobody";
+
+const reactElement = React.createElement(
+  "a", // first parameter expects tag eg. 'p', 'h1'
+  {
+    // expects properties
+    href: "https://youtube.com",
+    target: "_blank",
+  },
+  "Click Me To Open Youtube", // expects Text
+  anotherUser,
+  anotherElement,
+  MyApp()
+);
+
+createRoot(document.getElementById("root")).render(
+  // <MyApp />
+  // MyApp() // can also be called like this
+  // <ReactElement/> // Will no execute
+  // ReactElement // will not work
+  // anotherElement   // This works.
+  reactElement
+
+  // <App />
+  
+);
